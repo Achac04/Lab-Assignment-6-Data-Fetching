16cfb545fbdde434ae86e4d942e8807f98708043
Andrés Chacón <124219246+Achac04@users.noreply.github.com>
2024-02-04 19:50:13 -0700

    Upload Lab 2: Layout Components

---
+++ App.jsx
@@ -0,0 +1,21 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import TodoList from './TodoList';
+import ToDoForm from './ToDoForm';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <TodoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
